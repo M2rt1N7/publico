3b23fc62a7d2199773c6bc8b58eb241cfd331904
M2rt1N7 <74304811+M2rt1N7@users.noreply.github.com>
2021-05-13 13:27:16 +0200

    .

---
+++ login.js
@@ -0,0 +1,66 @@
+function leerXML() {
+          // lee desde GitHub.
+          var xhr = new XMLHttpRequest();
+          xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+              miFuncion(this);
+            }
+          };
+          xhr.open("GET", "https://M2rt1N7.github.io/publico/ProyectoFinalWeb/registrados.xml", true);
+          xhr.send();
+        }
+
+
+        function miFuncion(xml) {
+          var i;
+          var xmlDoc = xml.responseXML;
+          var x = xmlDoc.getElementsByTagName("usuario");
+          var checkingData = false;
+          var nombreUsuario = document.getElementById("usr").value;
+          var passUsuario =document.getElementById("clv").value;
+          for (i = 0; i < x.length; i++) {
+              if (x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue == nombreUsuario) {
+                  if (x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue == passUsuario) {
+                      checkingData = true;
+                      if (typeof(Storage) !== 'undefined') {
+                        sessionStorage.setItem("usuario",nombreUsuario);
+                        sessionStorage.setItem("clave",passUsuario);
+                        location.reload();
+                        break;
+                    } else {
+                        alert("El navegador no es compatible con SessionStorage.")
+                    }
+                  }
+                }
+              }
+              if(checkingData==true){
+                  alert("Bienvenido, "+ nombreUsuario)
+
+              }
+              }
+
+
+
+		function logOut() {
+			if (typeof (Storage) !== "undefined") {
+        if(confirm("¿Estás seguro?")){
+				sessionStorage.removeItem("usuario");
+        sessionStorage.removeItem("clave")
+        location.reload();
+			}
+			return false;
+		}
+  }
+
+    function checking(){
+      if(sessionStorage.getItem("usuario").length>0){
+        document.getElementById("login").style.display="none";
+        document.getElementById("logout").style.display="inline-block";
+        document.getElementById("conexionUsuario").innerHTML = sessionStorage.getItem("usuario");
+      }
+    }
+
+
+  window.onload = function launch(){
+      checking();
+    }
